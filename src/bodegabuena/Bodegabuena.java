4e452896b51afe117f2d4339b27376eff4e7dfa1
NATANAEL MORENO <NATANAEL MORENO@NATANAELMORENO>
2018-06-05 14:40:17 -0600

    bodega

---
+++ src/bodegabuena/Bodegabuena.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bodegabuena;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author NATANAEL MORENO
+ */
+public class Bodegabuena {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "hola mundo");
+    }
+    
+}
