b0505fa395607dd97665cf190f6bd5e6f7baa2c3
LeslieRoxana <leslieroxana21@gmail.com>
2018-06-19 11:14:02 -0600

    editado por leslie

--- src/bodegabuena/Bodegabuena.java
+++ src/bodegabuena/Bodegabuena.java
@@ -17,7 +17,7 @@ public class Bodegabuena {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "hola mundo");
+        JOptionPane.showMessageDialog(null, "hola mundo leslie");
     }
 
 }
